c3f814b93b55ca10465f66d65785d44ac5d12ad2
thomasdsambrook <81263780+thomasdsambrook@users.noreply.github.com>
2026-04-29 11:52:46 +0100

    Update base_template.py

--- base_template.py
+++ base_template.py
@@ -35,27 +35,12 @@ start_button.pack()
 window.mainloop()
 
 def read_trial_info():
-# reads a csv file with information and returns it as pandas dataframe trialinfo
-    #This tab is very important
-    #trialinfo=pd.DataFrame() #replace this, should be read from csv
-        #Tom note for whoever writes this bit...code in run_trial() assumes...
-        # a csv proposed by Nadja with fields PrimeWord; PrimeFile; TargetWord; Delay(ms); Relatedness. 
-        #Relatedness codes: REL = related word target, UNR = unrelated word target,NW  = non-word target
-        #It assumes also that csv will also have a field called CorrectResponse
-        #The experimenter option input bit above is neglected (I don't know what this represents) and is not currently returned in data{}
-    #This is a stub
-
-def read_trial_info():
-    """
-    The CSV file should be in the same folder as this Python script.
-    Expected columns:
-        PrimeWord
-        PrimeFile
-        TargetWord
-        Delay(ms)
-        Relatedness
-        CorrectResponse
-    """
+    # assumes a csv (in this directory) as proposed by Nadja with fields PrimeWord; PrimeFile; TargetWord; Delay(ms); Relatedness 
+    #Relatedness codes: REL = related word target, UNR = unrelated word target,NW  = non-word target
+    #It assumes also that csv will also have a field called CorrectResponse
+    #The experimenter option input bit above is neglected (I don't know what this represents) and is not currently returned in data{}
+    #below is the single bit of code in thsi function!
+    
     trialinfo = pd.read_csv("Nadjastimuli.csv")
     return trialinfo
 
